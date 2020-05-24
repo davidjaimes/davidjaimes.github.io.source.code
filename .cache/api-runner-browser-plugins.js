6dd2755c63b9c1df47d2a208da0b9145b70505d9
David Jaimes <david@djaimes.com>
2020-05-23 21:42:38 -0700

    complete layout

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
       plugin: require('../node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
       options: {"plugins":[],"defaultLayouts":{"default":"/Users/djaimes/davidjaimes.github.io.source.code/src/components/layout.jsx"},"extensions":[".mdx",".md"],"gatsbyRemarkPlugins":[{"resolve":"gatsby-remark-prismjs","options":{"classPrefix":"language-","inlineCodeMarker":null}}]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
     }]
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -3,7 +3,7 @@
 import React from 'react'
 
 // Bootstrap Dependencies
-import {Navbar, Nav, Container, Image} from 'react-bootstrap'
+import {Navbar, Nav, Container, Image, Row, Col} from 'react-bootstrap'
 import { FaGithub } from 'react-icons/fa'
 
 import Logo from "../images/logo.png"
@@ -11,19 +11,36 @@ import Logo from "../images/logo.png"
 export default function Navigation ({children}) {
 
   return (
-    <Navbar bg="dark" expand="lg" variant="dark">
-      <Container>
-        <Navbar.Brand href="/">
-          <Image src={Logo} style={{height: "30px", marginRight: "20px"}}/>David Jaimes
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-        <Navbar.Collapse id="responsive-navbar-nav" bg="dark" className="justify-content-end">
-          <Nav>
-            <Nav.Link href="/data">Data Analysis</Nav.Link>
-            <Nav.Link href="/macOS">macOS Configuration</Nav.Link>
-            <Nav.Link href="/github"><FaGithub /> GitHub</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <Container fluid>
+      <Row>
+        <Col className="col-12 col-lg-4 col-xl-3 bg-dark">
+          <Navbar bg="dark" expand="lg" variant="dark" className="align-items-start flex-lg-column flex-md-row">
+            <Navbar.Brand href="/" >
+              <Image src={Logo} style={{height: "30px", marginRight: "20px"}}/>David Jaimes
+            </Navbar.Brand>
+ 
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="flex-column">
+                <Nav.Link href="/github"><FaGithub size={25}/> GitHub</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
+
+        <Col>
+          <Container className="col-xl-10 pt-5 pb-5">
+            {children}
+          </Container>
+        </Col>
+
+      </Row>
+    </Container>
   )}
