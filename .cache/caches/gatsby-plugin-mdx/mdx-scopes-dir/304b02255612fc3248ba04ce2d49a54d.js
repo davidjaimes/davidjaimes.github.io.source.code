36f265be62120fa68f66b31aea6604bd02900ddc
David Jaimes <david@djaimes.com>
2020-05-30 11:25:49 -0700

    serve manifest variables to online presence

--- .cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/304b02255612fc3248ba04ce2d49a54d.js
+++
@@ -1,8 +0,0 @@
-import { Card } from "react-bootstrap";
-import { MountLaguna } from "../../../../src/images/mount-laguna.jpg";
-import * as React from 'react';
-export default {
-  Card,
-  MountLaguna,
-  React
-};
--- .cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/68e95850b09447dc2cadee06d9a349c3.js
+++
@@ -1,6 +0,0 @@
-import { Card } from "react-bootstrap";
-import * as React from 'react';
-export default {
-  Card,
-  React
-};
