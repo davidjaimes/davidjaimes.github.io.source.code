a12cd994c333fa5de610027022df1a4daea3b3a0
David Jaimes <david@djaimes.com>
2020-05-30 19:36:32 -0700

    finish python of zen

---
+++ .cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/cadd99f4930c70ea7eee117305547487.js
@@ -0,0 +1,16 @@
+import { Image, Badge, Card, Button, Row, Col, Alert } from "react-bootstrap";
+import { MountLaguna } from "../../../../src/images/mount-laguna.jpg";
+import Logo from "../../../../src/images/favicon.png";
+import * as React from 'react';
+export default {
+  Image,
+  Badge,
+  Card,
+  Button,
+  Row,
+  Col,
+  Alert,
+  MountLaguna,
+  Logo,
+  React
+};
