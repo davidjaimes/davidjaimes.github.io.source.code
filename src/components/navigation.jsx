45d40a0c1ef2258382853dfb875ae93fd9f722da
David Jaimes <david@djaimes.com>
2020-07-06 22:30:01 -0700

    add links for to navigation

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 
 // Bootstrap Dependencies
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
-import {FaGithub} from 'react-icons/fa'
+import {FaGithub, FaChalkboard} from 'react-icons/fa'
 
 
 export default function Navigation() {
 	return (
 		<Navbar collapseOnSelect expand="lg" className="shadow-sm">
 			<Navbar.Brand href="/">
-          <b>David Jaimes</b>
+          <FaChalkboard size={30}/> <b>David Jaimes</b>
 			</Navbar.Brand>
 			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 			<Navbar.Collapse id="responsive-navbar-nav">
 				<Nav className="mr-auto">
 					<Nav.Link href="/about">About</Nav.Link>
-					<Nav.Link href="/documentation">Documentation</Nav.Link>
+					<Nav.Link href="/gatsby-config">Documentation</Nav.Link>
+					<Nav.Link href="/legal">Terms of Use</Nav.Link>
 					<NavDropdown title="Tutorials" id="collasible-nav-dropdown">
 						<NavDropdown.Item href="/linear-regression">Linear Regression</NavDropdown.Item>
 						<NavDropdown.Item href="/eclipsing-binaries">Eclipsing Binaries</NavDropdown.Item>
