b492c48d99ac7944590cefcc40c4edf103495166
David Jaimes <david@djaimes.com>
2021-02-06 20:24:42 -0800

    update all

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -1,16 +1,16 @@
 // prefer default export if available
-const preferDefault = m => m && m.default || m
+const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---src-pages-about-index-mdx": () => import("./../src/pages/about/index.mdx" /* webpackChunkName: "component---src-pages-about-index-mdx" */),
-  "component---src-pages-eclipsing-binaries-index-mdx": () => import("./../src/pages/eclipsing-binaries/index.mdx" /* webpackChunkName: "component---src-pages-eclipsing-binaries-index-mdx" */),
-  "component---src-pages-gatsby-config-index-mdx": () => import("./../src/pages/gatsby-config/index.mdx" /* webpackChunkName: "component---src-pages-gatsby-config-index-mdx" */),
-  "component---src-pages-index-jsx": () => import("./../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */),
-  "component---src-pages-jskycalc-index-mdx": () => import("./../src/pages/jskycalc/index.mdx" /* webpackChunkName: "component---src-pages-jskycalc-index-mdx" */),
-  "component---src-pages-kepler-16-b-index-mdx": () => import("./../src/pages/kepler-16b/index.mdx" /* webpackChunkName: "component---src-pages-kepler-16-b-index-mdx" */),
-  "component---src-pages-legal-index-mdx": () => import("./../src/pages/legal/index.mdx" /* webpackChunkName: "component---src-pages-legal-index-mdx" */),
-  "component---src-pages-linear-regression-index-mdx": () => import("./../src/pages/linear-regression/index.mdx" /* webpackChunkName: "component---src-pages-linear-regression-index-mdx" */),
-  "component---src-pages-mac-os-config-index-mdx": () => import("./../src/pages/macOS-config/index.mdx" /* webpackChunkName: "component---src-pages-mac-os-config-index-mdx" */),
-  "component---src-pages-prior-index-mdx": () => import("./../src/pages/prior-index.mdx" /* webpackChunkName: "component---src-pages-prior-index-mdx" */)
+  "component---src-pages-about-index-mdx": () => import("./../../../src/pages/about/index.mdx" /* webpackChunkName: "component---src-pages-about-index-mdx" */),
+  "component---src-pages-eclipsing-binaries-index-mdx": () => import("./../../../src/pages/eclipsing-binaries/index.mdx" /* webpackChunkName: "component---src-pages-eclipsing-binaries-index-mdx" */),
+  "component---src-pages-gatsby-config-index-mdx": () => import("./../../../src/pages/gatsby-config/index.mdx" /* webpackChunkName: "component---src-pages-gatsby-config-index-mdx" */),
+  "component---src-pages-index-jsx": () => import("./../../../src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */),
+  "component---src-pages-jskycalc-index-mdx": () => import("./../../../src/pages/jskycalc/index.mdx" /* webpackChunkName: "component---src-pages-jskycalc-index-mdx" */),
+  "component---src-pages-kepler-16-b-index-mdx": () => import("./../../../src/pages/kepler-16b/index.mdx" /* webpackChunkName: "component---src-pages-kepler-16-b-index-mdx" */),
+  "component---src-pages-legal-index-mdx": () => import("./../../../src/pages/legal/index.mdx" /* webpackChunkName: "component---src-pages-legal-index-mdx" */),
+  "component---src-pages-linear-regression-index-mdx": () => import("./../../../src/pages/linear-regression/index.mdx" /* webpackChunkName: "component---src-pages-linear-regression-index-mdx" */),
+  "component---src-pages-mac-os-config-index-mdx": () => import("./../../../src/pages/macOS-config/index.mdx" /* webpackChunkName: "component---src-pages-mac-os-config-index-mdx" */),
+  "component---src-pages-prior-index-mdx": () => import("./../../../src/pages/prior-index.mdx" /* webpackChunkName: "component---src-pages-prior-index-mdx" */)
 }
 
